9b4726c9174353ab1a4bbffd684b8df8f5f68de8
indrajit <eendroroy@gmail.com>
2017-12-02 16:25:44 +0600

    update log report

--- git_reports/reports/log/log.py
+++ git_reports/reports/log/log.py
@@ -4,12 +4,15 @@ import os
 from pygit2 import Repository, GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 
 from git_reports.reports.__helpers import duration, commit_date
+from git_reports.reports.exporter.csv_exporter import CSVExporter
 
 
 class Log(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, export=False, output=None):
+        self.exportet = CSVExporter()
+        self.export = export
+        self.output = output
 
     @staticmethod
     def printer(obj):
@@ -22,16 +25,29 @@ class Log(object):
 
         repo = Repository('%s/.git' % os.getcwd())
         for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE):
-            self.printer(commit.author.name)
-            self.printer(' ==> ')
-            self.printer(commit_date(commit))
-            self.printer(' ==> ')
-            self.printer(commit.message.strip())
-            if last_commit is not None:
+            if not self.export:
+                self.printer(commit.author.name)
                 self.printer(' ==> ')
+                self.printer(commit_date(commit))
+                self.printer(' ==> ')
+                self.printer(commit.message.strip())
+            if self.export:
+                line = [commit.author.name, commit_date(commit), commit.message.strip()]
+            if last_commit is not None:
+                if not self.export:
+                    self.printer(' ==> ')
                 dur = duration(last_commit, commit)
-                self.printer('%d %d:%d:%d' % dur)
+                if not self.export:
+                    self.printer('%d %d:%d:%d' % dur)
+                if self.export:
+                    line.append('%d %d:%d:%d' % dur)
             if first_commit is None:
                 first_commit = commit
             last_commit = commit
-            print()
+            if not self.export:
+                print()
+            if self.export:
+                self.exportet.add_line(line)
+
+        if self.export:
+            self.exportet.write_content(self.output)
