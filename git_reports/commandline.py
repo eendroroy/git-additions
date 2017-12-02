74d444ac4e8a39e47c44519b664c06f2bb0e1158
indrajit <eendroroy@gmail.com>
2017-12-02 21:39:39 +0600

    update options

--- git_reports/commandline.py
+++ git_reports/commandline.py
@@ -13,18 +13,28 @@ class Cli(object):
     def __init__(self):
         self.parser = OptionParser()
         self.parser.add_option(
+            "-r", "--report", dest="report", help="name of the report", metavar="REPORT"
+        )
+        self.parser.add_option(
             "-o", "--out-file", dest="output", help="write report to FILE", metavar="FILE"
+        )
+        self.parser.add_option(
+            "-a", "--author", dest="author", help="filter by author name", metavar="AUTHOR"
+        )
+        self.parser.add_option(
+            "-e", "--email", dest="email", help="filter by author email", metavar="EMAIL"
         )
 
     def run(self):
         (options, args) = self.parser.parse_args()
-        report_name = args[0]
-        if report_name == 'log':
+        if options.report == 'log':
+            author = options.author
+            email = options.email
             if options.output is not None:
-                Log(export=True, output=options.output).run()
+                Log(author, email, True, options.output).run()
             else:
-                Log().run()
-        if report_name == 'stats':
+                Log(author, email).run()
+        if options.report == 'stats':
             Stats().run()
         try:
             sys.exit(0)
